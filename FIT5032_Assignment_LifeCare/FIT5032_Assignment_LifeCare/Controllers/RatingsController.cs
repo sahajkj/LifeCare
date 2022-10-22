c09d90e7688aa6dd151a3cf3c96b63188c05333e
sjai0024 <110817708+sjai0024@users.noreply.github.com>
2022-10-22 18:40:34 +1100

    Ratings

---
+++ FIT5032_Assignment_LifeCare/FIT5032_Assignment_LifeCare/Controllers/RatingsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using src.Models;
+
+namespace src.Controllers
+{
+    public class RatingsController : Controller
+    {
+        private LifecareDataModelContainer db = new LifecareDataModelContainer();
+
+        // GET: Ratings
+        public ActionResult Index()
+        {
+            var ratings = db.Ratings.Include(r => r.Doctor).Include(r => r.Patient);
+            return View(ratings.ToList());
+        }
+
+        // GET: Ratings/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rating rating = db.Ratings.Find(id);
+            if (rating == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rating);
+        }
+
+        // GET: Ratings/Create
+        public ActionResult Create()
+        {
+            ViewBag.DoctorId = new SelectList(db.Doctors, "Id", "Speciality");
+            ViewBag.PatientId = new SelectList(db.Patients, "Id", "Gender");
+            return View();
+        }
+
+        // POST: Ratings/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Value,DoctorId,PatientId")] Rating rating)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Ratings.Add(rating);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DoctorId = new SelectList(db.Doctors, "Id", "Speciality", rating.DoctorId);
+            ViewBag.PatientId = new SelectList(db.Patients, "Id", "Gender", rating.PatientId);
+            return View(rating);
+        }
+
+        // GET: Ratings/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rating rating = db.Ratings.Find(id);
+            if (rating == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DoctorId = new SelectList(db.Doctors, "Id", "Speciality", rating.DoctorId);
+            ViewBag.PatientId = new SelectList(db.Patients, "Id", "Gender", rating.PatientId);
+            return View(rating);
+        }
+
+        // POST: Ratings/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Value,DoctorId,PatientId")] Rating rating)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(rating).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DoctorId = new SelectList(db.Doctors, "Id", "Speciality", rating.DoctorId);
+            ViewBag.PatientId = new SelectList(db.Patients, "Id", "Gender", rating.PatientId);
+            return View(rating);
+        }
+
+        // GET: Ratings/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rating rating = db.Ratings.Find(id);
+            if (rating == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rating);
+        }
+
+        // POST: Ratings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Rating rating = db.Ratings.Find(id);
+            db.Ratings.Remove(rating);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
